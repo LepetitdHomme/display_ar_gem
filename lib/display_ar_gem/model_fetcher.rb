4b266c3273684fcc2f2ffa33e92f5bc8e39c7abf
LepetitdHomme <camille.sellier@skeepers.io>
2024-07-14 15:12:05 +0200

    selection ok

--- lib/display_ar_gem/model_fetcher.rb
+++ lib/display_ar_gem/model_fetcher.rb
@@ -1,21 +1,24 @@
 module DisplayArGem
   class ModelFetcher
     def self.fetch
-      models = ActiveRecord::Base.descendants
-      associations = models.each_with_object({}) do |model, hash|
-        hash[model.name] = model.reflect_on_all_associations.map do |assoc|
-          next unless assoc
+      # Load all models
+      Rails.application.eager_load!
 
+      # Specify the models you want to include
+      models =  ActiveRecord::Base.descendants.reject(&:abstract_class?)
+
+      associations = models.each_with_object({}) do |model, hash|
+        # puts "Processing model: \n\t#{model.name}" # Debugging line
+        hash[model.name] = model.reflect_on_all_associations.map do |assoc|
+          # puts "Processing association: #{assoc.name} for model: #{model.name}" # Debugging line
           begin
-            if assoc.is_a?(ActiveRecord::Reflection::ThroughReflection)
-              class_name = "Through (#{assoc.name})"
-            else
-              class_name = if assoc.polymorphic?
-                "Polymorphic (#{assoc.name})"
-              else
-                assoc.options[:class_name] || (assoc.klass.name if assoc.klass)
-              end || 'Unknown'
-            end
+            class_name = if assoc.is_a?(ActiveRecord::Reflection::ThroughReflection)
+                           "Through (#{assoc.name})"
+                         elsif assoc.polymorphic?
+                           "Polymorphic (#{assoc.name})"
+                         else
+                           assoc.options[:class_name] || assoc.klass&.name || 'Unknown'
+                         end
           rescue => e
             puts "Error while fetching association class_name: #{e.message}"
             class_name = 'Unknown'
@@ -26,8 +29,12 @@ module DisplayArGem
             type: assoc.macro,
             class_name: class_name
           }
-        end.compact
+        end
       end
+      # puts 'models:'
+      # puts models
+      # puts 'assoc:'
+      # puts associations
       { models: models, associations: associations }
     end
   end
--- lib/display_ar_gem/window.rb
+++ lib/display_ar_gem/window.rb
@@ -1,47 +1,218 @@
-require 'gosu'
+=begin
+
+{
+  "ActionText::RichText"=>[
+    {:name=>:record, :type=>:belongs_to, :class_name=>"Polymorphic (record)"},
+    {:name=>:embeds_attachments, :type=>:has_many, :class_name=>"ActiveStorage::Attachment"},
+    {:name=>:embeds_blobs, :type=>:has_many, :class_name=>"Through (embeds_blobs)"}
+  ],
+  "ActionText::EncryptedRichText"=>[
+    {:name=>:record, :type=>:belongs_to, :class_name=>"Polymorphic (record)"},
+    {:name=>:embeds_attachments, :type=>:has_many, :class_name=>"ActiveStorage::Attachment"},
+    {:name=>:embeds_blobs, :type=>:has_many, :class_name=>"Through (embeds_blobs)"}
+  ],
+  "ActiveStorage::VariantRecord"=>[
+    {:name=>:blob, :type=>:belongs_to, :class_name=>"ActiveStorage::Blob"},
+    {:name=>:image_attachment, :type=>:has_one, :class_name=>"ActiveStorage::Attachment"},
+    {:name=>:image_blob, :type=>:has_one, :class_name=>"Through (image_blob)"}
+  ],
+  "ActiveStorage::Blob"=>[
+    {:name=>:variant_records, :type=>:has_many, :class_name=>"ActiveStorage::VariantRecord"},
+    {:name=>:preview_image_attachment, :type=>:has_one, :class_name=>"ActiveStorage::Attachment"},
+    {:name=>:preview_image_blob, :type=>:has_one, :class_name=>"Through (preview_image_blob)"},
+    {:name=>:attachments, :type=>:has_many, :class_name=>"ActiveStorage::Attachment"}
+  ],
+  "ActiveStorage::Attachment"=>[
+    {:name=>:record, :type=>:belongs_to, :class_name=>"Polymorphic (record)"},
+    {:name=>:blob, :type=>:belongs_to, :class_name=>"ActiveStorage::Blob"}
+  ],
+  "ActionMailbox::InboundEmail"=>[
+    {:name=>:raw_email_attachment, :type=>:has_one, :class_name=>"ActiveStorage::Attachment"},
+    {:name=>:raw_email_blob, :type=>:has_one, :class_name=>"Through (raw_email_blob)"}
+  ],
+  "TaskLabel"=>[
+    {:name=>:task, :type=>:belongs_to, :class_name=>"Task"},
+    {:name=>:label, :type=>:belongs_to, :class_name=>"Label"}
+  ],
+  "Task"=>[
+    {:name=>:user, :type=>:belongs_to, :class_name=>"User"},
+    {:name=>:task_labels, :type=>:has_many, :class_name=>"TaskLabel"},
+    {:name=>:labels, :type=>:has_many, :class_name=>"Through (labels)"}
+  ],
+  "Label"=>[
+    {:name=>:user, :type=>:belongs_to, :class_name=>"User"},
+    {:name=>:task_labels, :type=>:has_many, :class_name=>"TaskLabel"},
+    {:name=>:tasks, :type=>:has_many, :class_name=>"Through (tasks)"}
+  ],
+  "User"=>[
+    {:name=>:tasks, :type=>:has_many, :class_name=>"Task"},
+    {:name=>:labels, :type=>:has_many, :class_name=>"Label"}
+  ]
+}
+
+=end
+
 
 module DisplayArGem
-	class Window < Gosu::Window
-		THROTTLE_INTERVAL = 3000
+  class Window < Gosu::Window
+    def initialize
+      super 800, 600
+      self.caption = "Models and Associations"
+      @background_color = Gosu::Color::WHITE
+      @model_color = Gosu::Color::GRAY
+      @text_color = Gosu::Color::BLACK
+      @assoc_color = Gosu::Color::RED
+      @highlight_color = Gosu::Color::YELLOW
+      @font = Gosu::Font.new(20)
+      @zoom_factor = 1.0
+      @offset_x = 0
+      @offset_y = 0
+      @dragging = false
+      @last_mouse_x = 0
+      @last_mouse_y = 0
 
-		def initialize
-			super 800, 600
-			self.caption = "ActiveRecord Models"
+      @models_data = DisplayArGem::ModelFetcher.fetch
+      @positions = calculate_positions(@models_data[:models])
 
-			@font = Gosu::Font.new(12)
+      @selected_model = nil
+    end
 
-			@models_data = DisplayArGem::ModelFetcher.fetch
-			@last_draw_time = Gosu.milliseconds
-			@text_buffer = Gosu.render(800, 600) do
-				draw_text(0, 0, "Initializing...")
+    def calculate_positions(models)
+      positions = {}
+      x, y = 50, 50
+      spacing_x, spacing_y = 150, 70
+      max_width = width - 100
+      models.each do |model|
+        if x > max_width
+          x = 50
+          y += spacing_y
+        end
+        positions[model.to_s] = { x: x, y: y }
+        x += spacing_x
       end
-		end
+      positions
+    end
 
-		def draw
-			@text_buffer.draw(0, 0, 0)
+    def draw
+      Gosu.draw_rect(0, 0, width, height, @background_color)
+      draw_models
+      draw_associations
+    end
 
-			@models_data[:models].each_with_index do |model, model_index|
-				draw_text(10, 20 + model_index * 20, model.name)
+    def draw_models
+      @positions.each do |model, pos|
+        color = model == @selected_model ? @highlight_color : @model_color
+        draw_model(model, pos[:x], pos[:y], color)
+      end
+    end
 
-				@models_data[:associations][model.name].each_with_index do |assoc, assoc_index|
-					draw_text(150, 20 + (model_index + assoc_index) * 20, "#{model} -> #{assoc[:name]} (#{assoc[:type]})")
-				end
-			end
-		end
+    def draw_model(model, x, y, color = @model_color)
+      width = (100 * @zoom_factor).round
+      height = (50 * @zoom_factor).round
+      Gosu.draw_rect((x * @zoom_factor + @offset_x).round, (y * @zoom_factor + @offset_y).round, width, height, color)
+      draw_text(model.to_s.split("::").last, (x * @zoom_factor + @offset_x + width / 2).round, (y * @zoom_factor + @offset_y + height / 2).round)
+    end
 
-		def update
-			current_time = Gosu.milliseconds
-			if current_time - @last_draw_time >= THROTTLE_INTERVAL
-				@last_draw_time = current_time
-			end
-		end
+    def draw_text(text, x, y)
+      @font.draw_text(text, x - @font.text_width(text) / 2, y - 10, 1, 1.0, 1.0, @text_color)
+    end
 
-		def draw_text(x, y, text)
-			@font.draw_text(text, x, y, 1)
-		end
+    def draw_associations
+      @models_data[:associations].each do |model, associations|
+        # binding.pry
+        start_pos = @positions[model]
+        associations.each do |assoc|
+          class_name = assoc[:class_name].split(' ').first
+          end_pos = @positions[class_name]
+          if start_pos && end_pos
+            color = (model == @selected_model || class_name == @selected_model) ? @highlight_color : @assoc_color
+            draw_arrow(
+              (start_pos[:x] * @zoom_factor + @offset_x + 50).round,
+              (start_pos[:y] * @zoom_factor + @offset_y + 25).round,
+              (end_pos[:x] * @zoom_factor + @offset_x + 50).round,
+              (end_pos[:y] * @zoom_factor + @offset_y + 25).round,
+              color
+              )
+            draw_association_label(assoc[:type], start_pos[:x], start_pos[:y], end_pos[:x], end_pos[:y])
+          end
+        end
+      end
+    end
 
-		def needs_cursor?
-			true
-		end
-	end
+    def button_down(id)
+      if id == Gosu::MsLeft
+        model_clicked = false
+        @positions.each do |model, pos|
+          if mouse_over_model?(mouse_x, mouse_y, pos[:x], pos[:y])
+            @selected_model = model
+            model_clicked = true
+            break
+          end
+        end
+        start_drag if model_clicked
+      end
+      handle_zoom(id)
+    end
+
+    def button_up(id)
+      stop_drag if id == Gosu::MsLeft
+    end
+
+    def update
+      if @dragging
+        delta_x = mouse_x - @last_mouse_x
+        delta_y = mouse_y - @last_mouse_y
+        @offset_x += delta_x
+        @offset_y += delta_y
+      end
+      @last_mouse_x = mouse_x
+      @last_mouse_y = mouse_y
+    end
+
+    def start_drag
+      @dragging = true
+      @last_mouse_x = mouse_x
+      @last_mouse_y = mouse_y
+    end
+
+    def stop_drag
+      @dragging = false
+    end
+
+    def handle_zoom(id)
+      case id
+      when Gosu::KbUp
+        @zoom_factor += 0.1
+      when Gosu::KbDown
+        @zoom_factor = [@zoom_factor - 0.1, 0.1].max
+      end
+    end
+
+    def mouse_over_model?(mouse_x, mouse_y, model_x, model_y)
+      x = (model_x * @zoom_factor + @offset_x).round
+      y = (model_y * @zoom_factor + @offset_y).round
+      width = (100 * @zoom_factor).round
+      height = (50 * @zoom_factor).round
+
+      mouse_x >= x && mouse_x <= (x + width) && mouse_y >= y && mouse_y <= (y + height)
+    end
+
+    def draw_association_label(type, x1, y1, x2, y2)
+      label_x = ((x1 + x2) / 2) * @zoom_factor
+      label_y = ((y1 + y2) / 2) * @zoom_factor
+      draw_text(type.to_s, label_x, label_y)
+    end
+
+    def draw_arrow(x1, y1, x2, y2, color = @assoc_color)
+      Gosu.draw_line(x1, y1, @assoc_color, x2, y2, color)
+      draw_arrowhead(x1, y1, x2, y2, color)
+    end
+
+    def draw_arrowhead(x1, y1, x2, y2, color = @assoc_color)
+      angle = Math.atan2(y2 - y1, x2 - x1)
+      size = 10
+      Gosu.draw_line(x2, y2, color, x2 - size * Math.cos(angle - Math::PI / 6), y2 - size * Math.sin(angle - Math::PI / 6), color)
+      Gosu.draw_line(x2, y2, color, x2 - size * Math.cos(angle + Math::PI / 6), y2 - size * Math.sin(angle + Math::PI / 6), color)
+    end
+  end
 end
